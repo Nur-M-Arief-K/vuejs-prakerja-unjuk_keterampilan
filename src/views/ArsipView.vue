80450be7f8b6a04a639293b7e8c2714ab561347b
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-09-17 23:09:16 +0700

    setup ArsipView HomeView

--- src/views/ArsipView.vue
+++ src/views/ArsipView.vue
@@ -1,3 +1,11 @@
 <template>
-  Arsip view
+  <v-row justify="center">
+    <v-col cols="12" md="6">
+      <papan-kategori-kegiatan title="Arsip" kategoriKegiatan="arsip" />
+    </v-col>
+  </v-row>
 </template>
+
+<script setup>
+  import PapanKategoriKegiatan from '@/components/PapanKategoriKegiatan.vue'
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,43 @@
 <template>
-  Home view
+  <v-container fluid>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      width="1024"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          v-bind="props"
+        >
+          + Tambahkan kegiatan
+        </v-btn>
+      </template>
+      <formulir-tambah-kegiatan :submit="() => dialog = false" :batalkan="() => dialog = false" />
+    </v-dialog>
+    
+    <v-row align="stretch">
+      <v-col cols="12" md="6">
+        <papan-kategori-kegiatan title="Keinginan" kategoriKegiatan="keinginan" />
+      </v-col>
+      <v-col cols="12" md="6">
+        <papan-kategori-kegiatan title="Berjalan" kategoriKegiatan="berjalan" />
+      </v-col>
+      <v-col cols="12" md="6">
+        <papan-kategori-kegiatan title="Selesai" kategoriKegiatan="selesai" />
+      </v-col>
+      <v-col cols="12" md="6">
+        <papan-kategori-kegiatan title="Ditunda" kategoriKegiatan="ditunda" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
 // @ is an alias to /src
+import PapanKategoriKegiatan from '@/components/PapanKategoriKegiatan.vue';
+import FormulirTambahKegiatan from '@/components/FormulirTambahKegiatan.vue';
+import { ref } from 'vue'
+
+const dialog= ref(false)
+
 </script>
