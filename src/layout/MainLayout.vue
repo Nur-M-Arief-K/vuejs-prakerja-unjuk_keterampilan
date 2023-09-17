ce00bf2d470af408b2954eecfcf99590e757860d
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-09-17 16:36:27 +0700

    create Views: Home, Arsip, DetailKegiatan; layout: MainLayout

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="isDrawerShown" temporary elevation="24">
+      <v-list nav>
+        <v-list-item :prepend-icon="mdiHome" title="Home" value="home" @click="navigateToHome()" />
+        <v-list-item :prepend-icon="mdiFolder" title="Arsip" value="arsip" @click="navigateToArsip()" />
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click.stop="isDrawerShown = !isDrawerShown.value" />
+      <v-app-bar-title text="Kanban Board" @click="navigateToHome()" tag="a" />
+    </v-app-bar>
+
+    <br />
+    <br />
+    <br />
+    <router-view />
+  </v-app>
+</template>
+
+<script setup>
+import { mdiHome, mdiFolder } from '@mdi/js';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const isDrawerShown = ref(false);
+const navigateToHome = () => router.push('/')
+const navigateToArsip = () => router.push('/arsip')
+</script>
+
+<style scoped>
+.v-app-bar-title:hover {
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  Home view
 </template>
 
-<script>
+<script setup>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
