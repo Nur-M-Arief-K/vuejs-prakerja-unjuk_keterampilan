d93ac37ad77a0810c4c49236cc41eeabb0300a5f
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-09-18 21:32:06 +0700

    add components and delete Test component

--- src/components/ItemKegiatan.vue
+++ src/components/ItemKegiatan.vue
@@ -1,48 +1,119 @@
 <template>
   <v-list-group>
     <template v-slot:activator="{ props }">
-      <v-list-item v-bind="props" title="Judul Kegiatan"></v-list-item>
+      <v-list-item v-bind="props" :title="namaKegiatan"></v-list-item>
     </template>
 
     <v-list-item>
-      <v-btn v-if="kategoriKegiatan != 'arsip'" size="x-small" text="Arsipkan" />
-      <v-btn size="x-small" text="Detail" />
+      <v-btn
+        v-if="kategoriKegiatan != 'Arsip'"
+        size="x-small"
+        text="Arsipkan"
+        @click="
+          pindahItemKegiatanHandler(kategoriKegiatan, indexKegiatan, 'Arsip')
+        "
+      />
+      <v-btn
+        size="x-small"
+        text="Detail"
+        @click="
+          navigasiKeDetailItemKegiatanHandler(kategoriKegiatan, indexKegiatan)
+        "
+      />
       <v-btn size="x-small">
         Pindahkan ke
         <v-menu activator="parent" location="end">
           <v-list>
-            <v-list-item :value="0" v-if="kategoriKegiatan != 'keinginan'">
+            <v-list-item
+              :value="0"
+              v-if="kategoriKegiatan != 'Keinginan'"
+              @click="
+                pindahItemKegiatanHandler(
+                  kategoriKegiatan,
+                  indexKegiatan,
+                  'Keinginan'
+                )
+              "
+            >
               <v-list-item-title>Keinginan</v-list-item-title>
             </v-list-item>
-            <v-list-item :value="1" v-if="kategoriKegiatan != 'berjalan'">
+            <v-list-item
+              :value="1"
+              v-if="kategoriKegiatan != 'Berjalan'"
+              @click="
+                pindahItemKegiatanHandler(
+                  kategoriKegiatan,
+                  indexKegiatan,
+                  'Berjalan'
+                )
+              "
+            >
               <v-list-item-title>Berjalan</v-list-item-title>
             </v-list-item>
-            <v-list-item :value="2" v-if="kategoriKegiatan != 'selesai'">
+            <v-list-item
+              :value="2"
+              v-if="kategoriKegiatan != 'Selesai'"
+              @click="
+                pindahItemKegiatanHandler(
+                  kategoriKegiatan,
+                  indexKegiatan,
+                  'Selesai'
+                )
+              "
+            >
               <v-list-item-title>Selesai</v-list-item-title>
             </v-list-item>
-            <v-list-item :value="3" v-if="kategoriKegiatan != 'ditunda'">
+            <v-list-item
+              :value="3"
+              v-if="kategoriKegiatan != 'Ditunda'"
+              @click="
+                pindahItemKegiatanHandler(
+                  kategoriKegiatan,
+                  indexKegiatan,
+                  'Ditunda'
+                )
+              "
+            >
               <v-list-item-title>Ditunda</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
       </v-btn>
     </v-list-item>
-
-    <v-list-item
-      v-for="([title], i) in admins"
-      :key="i"
-      :title="title"
-      :value="title"
-    ></v-list-item>
+    <v-list-item :title="logTerbaru" :value="logTerbaru"></v-list-item>
   </v-list-group>
 </template>
 
 <script setup>
-const open = ["Users"];
-const admins = [["[01/01/01] latest update"]];
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+
+const store = useStore();
+const router = useRouter();
 
 defineProps({
-  isArsip: Boolean,
   kategoriKegiatan: String,
-})
+  namaKegiatan: String,
+  logTerbaru: String,
+  indexKegiatan: Number,
+});
+
+const pindahItemKegiatanHandler = (
+  pindahDariKategoriKegiatan,
+  indexKegiatan,
+  pindahKeKategoriKegiatan
+) => {
+  store.commit("pindahItemKegiatan", {
+    pindahDariKategoriKegiatan,
+    indexKegiatan,
+    pindahKeKategoriKegiatan,
+  });
+};
+
+const navigasiKeDetailItemKegiatanHandler = (
+  kategoriKegiatan,
+  indexKegiatan
+) => {
+  router.push({ path: `/${kategoriKegiatan}/${indexKegiatan}` });
+};
 </script>
--- src/components/PapanKategoriKegiatan.vue
+++ src/components/PapanKategoriKegiatan.vue
@@ -1,22 +1,42 @@
 <template>
   <v-card>
     <v-card-item>
-      <v-card-title>{{title}}</v-card-title>
+      <v-card-title>{{ judulKategori }}</v-card-title>
     </v-card-item>
     <v-list>
-      <item-kegiatan :kategoriKegiatan="kategoriKegiatan" />
-      <item-kegiatan :kategoriKegiatan="kategoriKegiatan" />
-      <item-kegiatan :kategoriKegiatan="kategoriKegiatan" />
-      <item-kegiatan :kategoriKegiatan="kategoriKegiatan" />
-      <item-kegiatan :kategoriKegiatan="kategoriKegiatan" />
+      <item-kegiatan
+        v-for="(kegiatan, indexKegiatan) in daftarKegiatan"
+        :key="indexKegiatan"
+        :kategoriKegiatan="kategoriKegiatan"
+        :namaKegiatan="kegiatan.namaKegiatan"
+        :logTerbaru="getLogTerbaru({ kategoriKegiatan, indexKegiatan })"
+        :indexKegiatan="indexKegiatan"
+      />
     </v-list>
   </v-card>
 </template>
 
 <script setup>
-import ItemKegiatan from './ItemKegiatan.vue'
+import ItemKegiatan from "./ItemKegiatan.vue";
+import { useStore } from "vuex";
+import moment from "moment";
+
+const store = useStore();
+
 defineProps({
-  title: String,
+  judulKategori: String,
   kategoriKegiatan: String,
-})
+  daftarKegiatan: Array,
+});
+
+const getLogTerbaru = ({ kategoriKegiatan, indexKegiatan }) => {
+  const { judul, tanggal } = store.getters.getLogKemajuanTerbaru({
+    kategoriKegiatan,
+    indexKegiatan,
+  });
+
+  const formatTanggal = moment(tanggal).format("L");
+
+  return `[TERBARU] ${formatTanggal} ${judul}`;
+};
 </script>
